bcfa320557a0df05186bf3566cc212209cd41587
sinnesten <sinnesten@gmail.com>
2024-08-20 01:00:29 -0400

    v1

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     var location = "Home";
+    var locationIndex = "0";
     var goals = {
         "Obtain cake ingredient": false,
         "Train two of": {
@@ -9,15 +10,14 @@ $(document).ready(function () {
         },
         "Talk to Ali the Wise": false
     }
-    var time = 13;
+    var time = 14;
     var items = [];
-    var coins = 2000;
-    var achievements = [];
+    var coins = 10000;
+    var achievements = {};
 
     var currentOptions = {};
-    var index = $("#index").val();
-
-    // TODO: let player skip to end of day
+    var index = "0";
+    var isEpilogue = false;
     
     loadTextAndOptions(index);
     $("#input").on("keyup", processInput);
@@ -28,24 +28,40 @@ $(document).ready(function () {
         }
 
         var choice = (event.target.value).replace(".", "");
-        if (!Object.keys(currentOptions).includes(choice)) {
+        if ((time <= 0 || (choice.toLowerCase() === "end")) && !isEpilogue) {
+            beginEpilogue();
+            return;
+        }
+        else if (!Object.keys(currentOptions).includes(choice)) {
             $("#error").text("Sorry, I didn't understand that.");
             $("#input").val("");
             return;
         }
 
-        // TODO: implement tea handling
-
-        index = script[index]["options"][currentOptions[choice]][0];
+        if (index === "200") {
+            index = "201";
+        }
+        else if (script[index]["options"][currentOptions[choice]].length > 1) {
+            index = script[index]["options"][currentOptions[choice]].shift();
+        }
+        else {
+            index = script[index]["options"][currentOptions[choice]][0];
+        }
+        if (index === "-1") {
+            index = locationIndex;
+        }
+        else if (index === "1000") {
+            index = getEnding();
+        }
         loadTextAndOptions(index);
 
         if (index === "1" || Object.keys(script[index]).includes("goals")) {
-            // TODO: process goal change
+            updateGoals(script[index]["goals"]);
             loadGoals();
-            console.log("loaded");
         }
 
         if (["2", "3", "4"].includes(index)) {
+            loadCoins();
             loadTime();
         }
 
@@ -55,25 +71,68 @@ $(document).ready(function () {
         }
 
         if (Object.keys(script[index]).includes("gain")) {
-            items.push(script[index]["gain"]);
+            for (let item of script[index]["gain"]) {
+                if (!items.includes(item)) {
+                    items.push(item);
+                }
+            }
             loadItems();
         }
 
         if (Object.keys(script[index]).includes("loss")) {
-            if (script[index]["loss"].includes(" coins")) {
-                coins -= parseInt(script[index]["loss"].split(" ")[0]);
-            }
-            else {
-                items.splice(items.indexOf(script[index]["loss"]), 1);
+            for (let item of script[index]["loss"]) {
+                if (item.includes(" coins")) {
+                    coins -= parseInt(item.split(" ")[0]);
+                    loadCoins();
+                }
+                else if (items.includes(item)) {
+                    items.splice(items.indexOf(item), 1);
+                }
             }
             loadItems();
         }
 
         if (Object.keys(script[index]).includes("achievement")) {
-            achievements.push(script[index]["achievement"]);
+            achievements[script[index]["achievement"][0]] = script[index]["achievement"][1];
         }
 
         $("#input").val("");
+    }
+
+    function beginEpilogue() {
+        $("#time").hide();
+        $("#input").val("");
+        choice = "1";
+        index = "200";
+        isEpilogue = true;
+        
+        loadTextAndOptions(index);
+    }
+
+    function getEnding() {
+        var numSkills = 0;
+        for (let goal in goals["Train two of"]) {
+            if (goals["Train two of"][goal] === true) {
+                numSkills++;
+            }
+        }
+
+        if (numSkills < 2) {
+            return "207";
+        }
+        else if (goals["Talk to Ali the Wise"] === false) {
+            return "208";
+        }
+        else {
+            return "209";
+        }
+    }
+
+    function loadAchievements() {
+        for (let achievement in achievements) {
+            $("#text").append("<hr><p><b>" + achievement + "</b><p>");
+            $("#text").append("<p>" + achievements[achievement] + "<p>");
+        }
     }
 
     function loadTextAndOptions(index) {
@@ -84,6 +143,7 @@ $(document).ready(function () {
 
         if (Object.keys(script[index]).includes("location")) {
             location = script[index]["location"];
+            locationIndex = index;
         }
         $("#location").text(location);
 
@@ -94,25 +154,76 @@ $(document).ready(function () {
             $("#text").append("<p>" + paragraph + "</p>");
         }
 
-        // TODO: hide "item" options if the player does not have the required item
+        if (index === "210") {
+            loadAchievements();
+            $("#input").hide();
+        }
+
         var i = 1;
         for (let option of Object.keys(options)) {
+            // prevent user from selecting options they don't have the resources for
+            if (option.includes("[")) {
+                var startIndex = option.search("\\[") + 1;
+                var endIndex = option.search("\\]");
+                if (option.includes("coins]")) {
+                    requiredCoins = parseInt(option.substring(startIndex, endIndex - 6));
+                    if (coins < requiredCoins) {
+                        continue;
+                    }
+                }
+                else {
+                    var requiredItem = option.substring(startIndex, endIndex);
+                    if (!items.includes(requiredItem)) {
+                        continue;
+                    }
+                }
+            }
+
             $("#text-options").append("<p>" + i + ". " + option + "</p>");
             currentOptions[i] = option;
+            if (isEpilogue) {
+                break; // epilogue is linear, only one available choice
+            }
             i++;
         }
     }
 
+    function updateGoals(finishedGoal) {
+        for (let goal in goals) {
+            if (goal === finishedGoal) {
+                goals[goal] = true;
+                return;
+            }
+            if (typeof goals[goal] === "object") {
+                for (let subGoal in goals[goal]) {
+                    if (subGoal === finishedGoal) {
+                        goals[goal][subGoal] = true;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
     function loadGoals() {
-        // TODO: strike through complete goals
         $("#goals").empty();
         goalString = "";
         for (let goal in goals) {
-            goalString += "<li>" + goal;
+            if (goals[goal] === true) {
+                goalString += "<li style=\"text-decoration: line-through !important\">" + goal;
+            }
+            else {
+                goalString += "<li>" + goal;
+            }
             if (typeof goals[goal] === "object") {
                 goalString += ":<ul>";
                 for (let subGoal in goals[goal]) {
-                    goalString += "<li>" + subGoal + "</li>";
+                    if (goals[goal][subGoal] === true) {
+                        goalString += "<li style=\"text-decoration: line-through !important\">" + subGoal + "</li>";
+                    }
+                    else {
+                        goalString += "<li>" + subGoal + "</li>";
+                    }
                 }
                 goalString += "</ul>";
             }
@@ -123,13 +234,32 @@ $(document).ready(function () {
 
     function loadItems() {
         $("#items").empty();
+        items.sort();
         for (let item of items) {
             $("#items").append("<li>" + item + "</li>");
         }
     }
 
+    function loadCoins() {
+        $("#coins").text("Coins: " + coins);
+    }
+
     function loadTime() {
-        // TODO: format time
-        $("#time").text("Time left: " + Math.min(12, time));
+        if (time <= 0) {
+            $("#time").text("Time left: 0 minutes");
+            return;
+        }
+
+        var partialHours = time - Math.floor(time);
+        if (Math.floor(partialHours) > 0) {
+            $("#time").text("Time left: " + (partialHours * 60) + " minutes");
+        }
+        else {
+            var formattedTime = Math.floor(time) === 1 ? "1 hour" : Math.floor(time) + " hours";
+            if (partialHours > 0) {
+                formattedTime += " " + (partialHours * 60) + " minutes";
+            }
+            $("#time").text("Time left: " + formattedTime);
+        }
     }
 });
